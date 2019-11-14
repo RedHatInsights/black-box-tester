6ee2c7f7e06055f743501ac3342e2766b2302e94
Ben Turner <bturner@redhat.com>
2019-11-14 15:23:45 -0500

    Added latest cachet updater
    Signed-off-by: Ben Turner <bturner@redhat.com>

--- cachet-tools/cachet_update_daemon.py
+++ cachet-tools/cachet_update_daemon.py
@@ -10,16 +10,16 @@ export CACHET_URL="cachet.cachet.svc"
 export CACHET_TOKEN="fXPydfUD3c1aBUGiQnSb"
 """
 import json
-import requests
 import os
 import logging
+import requests
 from flask import Flask, request
 
 APP = Flask(__name__)
 CACHET_IDS = ""
 CACHET_URL = os.getenv("CACHET_URL")
 CACHET_TOKEN = os.getenv("CACHET_TOKEN")
-logging.basicConfig(level='INFO',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(level='INFO', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 logger = logging.getLogger(__name__)
 
 @APP.route('/', methods=['POST'])
@@ -34,37 +34,37 @@ def handle_alert():
     logging.info("Dumping incoming JSON from Alertmanager:")
     logging.info(str(json.dumps(alert_in, indent=4)))
     status = alert_in["status"]
-    logging.info("Root Status is: " + str(status))
+    logging.info("Root Status is: %s", str(status))
     for status_in in alert_in["alerts"]:
         plugin_name = status_in['labels']['plugin']
-        logging.info("Plugin Name: " + plugin_name)
-        logging.info("Plugin Status: " + str(status_in['status']))
+        logging.info("Plugin Name: %s", str(plugin_name))
+        logging.info("Plugin Status: %s", str(status_in['status']))
         CACHET_IDS = build_cachet_map()
-        logging.info("Cachet IDs: " + str(CACHET_IDS))
+        logging.info("Cachet IDs: %s", str(CACHET_IDS))
         component_number = search_component(plugin_name.capitalize(), CACHET_IDS)
         if component_number == 9999:
             logging.error("Component not found!")
             return "FAIL"
-        logging.info("Component Number: " + str(component_number))
+        logging.info("Component Number: %s", str(component_number))
         if str(status_in['status']) == "firing":
             output = update_component(component_number, 4)
         elif str(status_in['status']) == "resolved":
             output = update_component(component_number, 1)
         else:
             output = update_component(component_number, 1)
-        logging.info("Output: " + str(output))
+        logging.info("Output: %s", str(output))
     return "OK"
 
 def build_cachet_map():
-    """Builds a map of the components in the Cachet instance, returns 
+    """Builds a map of the components in the Cachet instance, returns
     the number that represents that component.
     """
     url = "http://" + str(CACHET_URL) + "/api/v1/components/?per_page=1000"
     try:
         req = requests.get(url)
         return {component["name"]: component["id"] for component in req.json()["data"]}
-    except requests.exceptions.RequestException as e:
-        logging.exception(e)
+    except requests.exceptions.RequestException as map_exception:
+        logging.exception(map_exception)
 
 def search_component(key, CACHET_IDS):
     """ Searches the list of componets in cachet for the
@@ -84,9 +84,9 @@ def update_component(component_id, status):
     data = '{"status":"' + str(status) + '"}'
     try:
         ret = requests.put(url, data=data, headers=headers, verify=False)
-    except requests.exceptions.RequestException as e:
-        logging.error("Return Code = " + str(ret.status_code))
-        logging.exception(e)
+    except requests.exceptions.RequestException as update_exception:
+        logging.error("Return Code = %s", str(ret.status_code))
+        logging.exception(update_exception)
     return 0
 
 if __name__ == '__main__':
